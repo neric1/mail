84a53eeeccff54ce200500b64769af322c22c471
Eric Ngendahimana <91191732+neric1@users.noreply.github.com>
2025-04-06 15:00:22 +0200

    specify add

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/neric1/mail/controller"
 )
@@ -14,11 +15,19 @@ func NewRouter(
 	router := gin.Default()
 	// add swagger
 	// CORS middleware
-	router.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
-		c.Next()
-	})
+	// router.Use(func(c *gin.Context) {
+	// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+	// 	c.Next()
+	// })
+	corsConfig := cors.Config{
+		AllowOrigins:     []string{"https://idsr.afro.who.int"},     // Allow only this origin
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},        // Allow only these HTTP methods
+		AllowHeaders:     []string{"Content-Type", "Authorization"}, // Allow these headers
+		AllowCredentials: true,                                      // Allow credentials (cookies, authorization headers, etc.)
+	}
+
+	router.Use(cors.New(corsConfig))
 	router.GET("", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, "Mailer: welcome home")
 	})
